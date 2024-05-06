fb46e8d2b27968d21a86a6f303cd856bf7c8893d
dominikbrezovsek <dominik.brezovsek.17339@dijak.sc-celje.si>
2024-05-06 13:14:38 +0200

    Changes to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CronController;
+use App\Http\Controllers\OrganisationAdminApiController;
 use App\Http\Controllers\PasswordResetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('/OAD/')->group(function () {
-    Route::post('login/', [\App\Http\Controllers\OrganisationAdminApiController::class, 'login']);
+    Route::post('login/', [OrganisationAdminApiController::class, 'login']);
     Route::middleware('OAD_API')->group(function () {
-
+        Route::post('/getUser/', [OrganisationAdminApiController::class, 'getUser']);
     });
 });
 
